3716235ac77cafd817b26b5218a95bec7ec706e2
Njagah <mac@MACs-MacBook-Pro-2.local>
2024-05-31 13:20:32 +0300

    changes

--- src/preloader/Preloader.jsx
+++ src/preloader/Preloader.jsx
@@ -3,8 +3,32 @@ import React from "react";
 const Preloader = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white z-50">
-      <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-b-4 border-gray-900">
-      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        className="animate-spin"
+      >
+        <path
+          fill="currentColor"
+          d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z"
+          opacity="0.5"
+        />
+        <path
+          fill="currentColor"
+          d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="1s"
+            from="0 12 12"
+            repeatCount="indefinite"
+            to="360 12 12"
+            type="rotate"
+          />
+        </path>
+      </svg>
     </div>
   );
 };
