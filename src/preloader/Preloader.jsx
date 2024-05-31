1801d0323ebaa755654de4ea93b7dd059317436f
Njagah <mac@MACs-MacBook-Pro-2.local>
2024-05-31 15:40:40 +0300

    changes

--- src/preloader/Preloader.jsx
+++ src/preloader/Preloader.jsx
@@ -2,32 +2,46 @@ import React from "react";
 
 const Preloader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white z-50">
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#d0dede] z-50">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
+        width="88"
+        height="88"
         viewBox="0 0 24 24"
-        className="animate-spin"
       >
-        <path
-          fill="currentColor"
-          d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z"
-          opacity="0.5"
-        />
-        <path
-          fill="currentColor"
-          d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"
-        >
-          <animateTransform
-            attributeName="transform"
-            dur="1s"
-            from="0 12 12"
+        <circle cx="18" cy="12" r="0" fill="currentColor">
+          <animate
+            attributeName="r"
+            begin=".67"
+            calcMode="spline"
+            dur="1.5s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
             repeatCount="indefinite"
-            to="360 12 12"
-            type="rotate"
+            values="0;2;0;0"
           />
-        </path>
+        </circle>
+        <circle cx="12" cy="12" r="0" fill="currentColor">
+          <animate
+            attributeName="r"
+            begin=".33"
+            calcMode="spline"
+            dur="1.5s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="6" cy="12" r="0" fill="currentColor">
+          <animate
+            attributeName="r"
+            begin="0"
+            calcMode="spline"
+            dur="1.5s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
       </svg>
     </div>
   );
