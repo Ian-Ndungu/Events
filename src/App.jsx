5774556338c2d10cde6b54718ef17e45c99268c6
Njagah <mac@MACs-MacBook-Pro-2.local>
2024-05-31 13:07:01 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,35 @@ import Contact from "./pages/contact/Contact";
 import Blog from "./pages/blog/Blog";
 import Ticket from "./pages/ticket/Ticket";
 import Eventplan from "./pages/eventplan/Eventplan";
+import { useEffect, useState } from "react";
+import Preloader from "./preloader/Preloader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/user-layout/about" element={<About />} />
-          <Route path="/user-layout/contactus" element={<Contact />} />
-          <Route path="/user-layout/blog" element={<Blog />} />
-          <Route path="user-layout/ticket-booking" element={<Ticket />} />
-          <Route path="/user-layout/event-planning" element={<Eventplan />} />
-        </Route>
-      </Routes>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/user-layout/about" element={<About />} />
+            <Route path="/user-layout/contactus" element={<Contact />} />
+            <Route path="/user-layout/blog" element={<Blog />} />
+            <Route path="user-layout/ticket-booking" element={<Ticket />} />
+            <Route path="/user-layout/event-planning" element={<Eventplan />} />
+          </Route>
+        </Routes>
+      )}
     </>
   );
 };
